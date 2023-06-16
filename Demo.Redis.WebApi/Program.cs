b8a302e8127bd1684bd21037dca1fa3102103da1
Doug Murphy <doug.murphy82@gmail.com>
2023-06-16 19:13:00 -0400

    Add XML comments to Swagger.

--- Demo.Redis.WebApi/Program.cs
+++ Demo.Redis.WebApi/Program.cs
@@ -1,6 +1,7 @@
 using Demo.Redis.Infrastructure.Postgres;
 using Demo.Redis.Infrastructure.Redis;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 builder.Services.AddDbContext<PersonContext>();
 builder.Services.AddScoped<PersonsRepository>();
